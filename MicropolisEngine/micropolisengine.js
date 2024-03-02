04ab12927df3906f235041e8923d8a9dbc6ebb7b
SimHacker <SimHacker@users.noreply.github.com>
2024-03-02 17:38:01 +0000

    deploy: 782f11658ccd59ff5b754de178c0690eab050400

--- MicropolisEngine/micropolisengine.js
+++ MicropolisEngine/micropolisengine.js
@@ -16,7 +16,7 @@ var Module = typeof Module != 'undefined' ? Module : {};
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpwgkl1xph.js
+// include: /tmp/tmp6o8hw6qs.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -195,21 +195,21 @@ Module['FS_createPath']("/", "cities", true, true);
 
   })();
 
-// end include: /tmp/tmpwgkl1xph.js
-// include: /tmp/tmpyqh08tdb.js
+// end include: /tmp/tmp6o8hw6qs.js
+// include: /tmp/tmp9dl44790.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['ENVIRONMENT_IS_PTHREAD'] || Module['$ww']) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpyqh08tdb.js
-// include: /tmp/tmpz2ryud54.js
+  // end include: /tmp/tmp9dl44790.js
+// include: /tmp/tmpex_ndnwz.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpz2ryud54.js
+  // end include: /tmp/tmpex_ndnwz.js
 
 
 // Sometimes an existing Module object exists with properties
---
+++ _app/immutable/entry/start.B1ifQeag.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D0_xOdDo.js";export{t as start};
