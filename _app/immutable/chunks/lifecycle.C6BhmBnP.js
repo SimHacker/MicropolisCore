07146c962f798dc6553280d5bbc5a1bfe93e30be
SimHacker <SimHacker@users.noreply.github.com>
2024-06-15 10:15:48 +0000

    deploy: 3530494e34b7e428456bfa59abd881cdf768a158

---
+++ _app/immutable/chunks/lifecycle.C6BhmBnP.js
@@ -0,0 +1,1 @@
+import{u as c,i as t,g as i,r as a,j as l,k as u,l as e,m}from"./runtime.DsU27fnY.js";function p(){const n=i,o=n.l.u;o&&(o.b.length&&c(()=>{f(n),a(o.b)}),t(()=>{const r=l(()=>o.m.map(u));return()=>{for(const s of r)typeof s=="function"&&s()}}),o.a.length&&t(()=>{f(n),a(o.a)}))}function f(n){if(n.l.s)for(const o of n.l.s)e(o);m(n.s)}export{p as i};
